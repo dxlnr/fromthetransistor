7e6172a98affa3d7ba2789ddfef104bb267cb3ff
dxlnr <daniel.illner@outlook.de>
2023-10-22 23:34:57 +0200

    ast kind of there

--- 04/compiler/compiler.hs
+++ 04/compiler/compiler.hs
@@ -6,26 +6,28 @@ import Data.Char (isDigit, isLetter, toUpper, isAlpha)
 
 ---------------------------------------------------------
 -- Lexer
-data Toks = SDO 
-          | SELSE
-          | SIF
-          | SWHILE
-          | LBRA
-          | RBRA
-          | LPAR
-          | RPAR
-          | PLUS
-          | MINUS
-          | LESS
-          | SEMI
-          | EQUAL
-          | INT
-          | ID
-          | EOL
-          | NONE
+data Tokid = SDO 
+           | SELSE
+           | SIF
+           | SWHILE
+           | LBRA
+           | RBRA
+           | LPAR
+           | RPAR
+           | PLUS
+           | MINUS
+           | LESS
+           | SEMI
+           | EQUAL
+           | INT
+           | ID
+           | EOL
+           | NONE
         deriving (Show, Enum, Bounded, Eq)
 
-kwords :: [(Maybe String, Toks)]
+data Toks = Toks { tok :: Tokid, v :: String } deriving (Show, Eq)
+
+kwords :: [(Maybe String, Tokid)]
 kwords =  [
     (Just "do", SDO), 
     (Just "else", SELSE),
@@ -34,11 +36,11 @@ kwords =  [
     (Nothing, ID)
     ]
 
-isKword :: String -> Maybe Toks
+isKword :: String -> Maybe Tokid
 isKword w = 
     lookup (Just w) kwords
 
-mToks :: Maybe Toks -> Toks
+mToks :: Maybe Tokid -> Tokid
 mToks (Just x) = x
 mToks Nothing  = ID 
 
@@ -58,25 +60,25 @@ iWhileDigit (x:xs)
         in (x:w, after)
     | otherwise = ([], x:xs)
 
-lx :: [Char] -> [(Toks, String)] 
+lx :: [Char] -> [Toks] 
 lx [] = []
 lx (c:cs)
     | (isDigit c)  = 
         let (d, after) = iWhileDigit (c:cs)
-        in (INT, d)  : lx after
-    | (c == '{')   = (LBRA,  [c]) : lx cs
-    | (c == '}')   = (RBRA,  [c]) : lx cs
-    | (c == '(')   = (LPAR,  [c]) : lx cs
-    | (c == ')')   = (RPAR,  [c]) : lx cs
-    | (c == ';')   = (SEMI,  [c]) : lx cs
-    | (c == '+')   = (PLUS,  [c]) : lx cs
-    | (c == '-')   = (MINUS, [c]) : lx cs
-    | (c == '=')   = (EQUAL, [c]) : lx cs
-    | (c == '<')   = (LESS,  [c]) : lx cs
+        in Toks INT d  : lx after
+    | (c == '{')   = Toks LBRA  [c] : lx cs
+    | (c == '}')   = Toks RBRA  [c] : lx cs
+    | (c == '(')   = Toks LPAR  [c] : lx cs
+    | (c == ')')   = Toks RPAR  [c] : lx cs
+    | (c == ';')   = Toks SEMI  [c] : lx cs
+    | (c == '+')   = Toks PLUS  [c] : lx cs
+    | (c == '-')   = Toks MINUS [c] : lx cs
+    | (c == '=')   = Toks EQUAL [c] : lx cs
+    | (c == '<')   = Toks LESS  [c] : lx cs
     | (isLetter c) = 
         let (w, after) = iWhileAlpha (c:cs)
-        in (mToks (isKword w), w) : lx after
-    | (c == '\n')  = (EOL, [c])   : lx cs
+        in Toks (mToks (isKword w)) w : lx after
+    | (c == '\n')  = Toks EOL [c]   : lx cs
     | otherwise    = lx cs 
 
 ---------------------------------------------------------
@@ -95,101 +97,79 @@ data Sym = VAR
          | SEQ
          | EXPR
          | PROG
-        deriving (Show, Enum, Bounded)
+        deriving (Show, Enum, Bounded, Eq)
 
--- data Node = Node { kind :: Sym, o1 :: Maybe Node, o2 :: Maybe Node } deriving (Show)
-data AST a = Empty
-           | Node a [AST a] String
-        deriving (Show, Eq)
+data Node = Node { 
+        kind :: Sym, 
+        o1   :: Maybe Node, 
+        o2   :: Maybe Node, 
+        str  :: String 
+    } deriving (Show)
 
--- insert :: a -> AST a -> AST a
--- insert x Empty = Node x []
--- insert x (Node y childs) = Node y (childs ++ [Node x []])
+-- data AST a = Empty
+--            | Node a [AST a] String
+--         deriving (Show, Eq)
 
-insert :: AST a -> AST a -> AST a
-insert x  Empty = x 
-insert x (Node y children v) = Node y (x: children) v
+---- insert :: a -> AST a -> AST a
+---- insert x Empty = Node x []
+---- insert x (Node y childs) = Node y (childs ++ [Node x []])
+
+-- insert :: AST a -> AST a -> AST a
+-- insert x  Empty = x 
+-- insert x (Node y children v) = Node y (x: children) v
 
 -- <term> ::= <id> | <int> | <paren_expr>
-pterm :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
-pterm [] = (Empty, [])
-pterm t@((toks, str):ts) 
+pterm :: [Toks] -> (Node, [Toks])
+pterm tt@(t:ts) 
     | trace ("pterm " ++ show t) False = undefined 
-    | (toks == ID)  = (Node VAR [] str, ts) 
-    | (toks == INT) = (Node CST [] str, ts) 
-    | otherwise     = (Node SET [] str, (ts))
+    | (tok t == ID)  = (Node VAR Nothing Nothing (v t), ts) 
+    | (tok t == INT) = (Node CST Nothing Nothing (v t), ts) 
+    | otherwise = expr ts
 
 -- <sum> ::= <term> | <sum> "+" <term> | <sum> "-" <term>
-psum :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
-psum [] = (Empty, [])
-psum ((toks, str):ts) 
-    | otherwise = pterm ((toks, str):ts) 
+psum :: [Toks] -> (Node, [Toks])
+psum (t:ts) 
+    | otherwise = pterm (t:ts) 
 
 -- <test> ::= <sum> | <sum> "<" <sum>
-ptest :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
-ptest [] = (Empty, [])
-ptest ((toks, str):ts) 
-     -- | (toks == LESS) = (Node LESSTHAN [] "", ts) 
-     | otherwise = psum ((toks, str):ts) 
+ptest :: [Toks] -> (Node, [Toks])
+ptest (t:ts) =
+    let (n, after) = psum (t:ts) 
+    in if tok t == LESS
+       then 
+           let (n2, after2) = psum (t:ts)
+           in (Node LESSTHAN (Just n) (Just n2) "<", after2)
+       else (n, after)
 
 -- <expr> ::= <test> | <id> "=" <expr>
-expr :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
-expr [] = (Empty, [])
-expr t@((toks, str):ts)
-    | trace ("expr " ++ show t) False = undefined 
-    -- | (toks /= ID) = 
-    --     let (n, after) = ptest ((toks, str):ts)
-    --     in  (n, after)
-    | otherwise =  
-        let na@(n, after) = ptest ((toks, str):ts)
-            ast = insert n (fst (expr after))
-        in trace ("expr na: " ++ show na) (ast, snd (expr after)) 
---
--- expr :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
--- expr [] = (Empty, [])
--- expr ((toks, str):ts) 
---     | (toks /= ID) = ptest ((toks, str):ts)
---     | otherwise = 
---     -- let (n, after) = ptest ((toks, str):ts)
---         -- (nextAST, remainingTokens) = expr after
---     let (currentAST, afterCurrent) = ptest ((toks, str):ts)
---         (nextAST1, afterNext1) = expr afterCurrent
---         (nextAST2, remainingTokens) = expr afterNext1
---     in if (toks == ID) 
---         then let combinedAST1 = insert currentAST nextAST1
---                  combinedAST2 = insert combinedAST1 nextAST2
---              in (combinedAST2, remainingTokens)
---         -- then (insert n nextAST, remainingTokens)
---         -- else (n, ts)
---         else (Node EMPTY [] "", ts)
+expr :: [Toks] -> (Node, [Toks])
+expr (t:ts)
+    | tok t /= ID = ptest (t:ts) 
+    | otherwise =
+        let (n, after) = ptest (t:ts) in
+        if kind n == VAR && tok (head after) == EQUAL
+            then let (rhs, more) = expr (after) in
+                 (Node SET (Just n) (Just rhs) "=", (t:more))
+            else (n, after)
 
--- expr :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
--- expr [] = (Empty, [])
--- expr ((toks, str):ts)
---     | toks /= ID = 
---         let (parsedNode, remaining) = ptest ((toks, str):ts)
---         in (parsedNode, remaining)
---     | toks == ID = 
---         let (currentAST, afterCurrent) = ptest ((toks, str):ts)
---             (nextAST1, afterNext1) = expr afterCurrent
---             (nextAST2, remainingTokens) = expr afterNext1
---             combinedAST1 = insert currentAST nextAST1
---             combinedAST2 = insert combinedAST1 nextAST2
---         in (combinedAST2, remainingTokens)
+stmt :: [Toks] -> (Node, [Toks]) 
+stmt (t:ts)
+     | (tok t == SIF)    = stmt ts
+     | (tok t == SDO)    = stmt ts
+     | (tok t == SWHILE) = stmt ts
+     | (tok t == LBRA)   = stmt ts
+     | (tok t == SEMI)   = stmt ts
+     | otherwise = 
+        let (n, after) = expr (t:ts)
+        in (Node EXPR (Just n) Nothing "", ts)
 
-
-par :: [(Toks, String)] -> AST Sym
-par [] = Node PROG [] ""
-par ((toks, str):ts)
-    | (toks == SIF)    = par ts
-    | (toks == SDO)    = par ts
-    | (toks == SWHILE) = par ts
-    | (toks == LBRA)   = par ts
-    | (toks == SEMI)   = par ts
-    | otherwise = 
-        let (n, after) = (expr ((toks, str):ts))
-        in insert n (par after)
-        -- if (toks /= SEMI) then error "syntax error"
+par :: [Toks] -> Node 
+par [] = error "Syntax error." 
+par (t:ts)
+    | (tok t == EOL) = error "syntax error"
+    | otherwise =
+        let (n, after) = stmt (t:ts)
+        in Node PROG (Just n) Nothing ""
 
 ---------------------------------------------------------
 -- Code generator

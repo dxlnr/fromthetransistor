2c892d8167b80b5bb729af5961b70f23267dfccd
dxlnr <daniel.illner@outlook.de>
2023-10-22 19:41:48 +0200

    uff

--- 04/compiler/compiler.hs
+++ 04/compiler/compiler.hs
@@ -1,3 +1,4 @@
+import Debug.Trace
 import System.Environment (getArgs)
 import System.IO (openFile, IOMode(ReadMode), hGetContents)
 import Data.Typeable (typeOf)
@@ -84,7 +85,7 @@ data Sym = VAR
          | CST
          | ADD
          | SUB
-         | LT
+         | LESSTHAN
          | SET
          | IF1
          | IF2
@@ -98,7 +99,7 @@ data Sym = VAR
 
 -- data Node = Node { kind :: Sym, o1 :: Maybe Node, o2 :: Maybe Node } deriving (Show)
 data AST a = Empty
-           | Node a [AST a] 
+           | Node a [AST a] String
         deriving (Show, Eq)
 
 -- insert :: a -> AST a -> AST a
@@ -107,20 +108,84 @@ data AST a = Empty
 
 insert :: AST a -> AST a -> AST a
 insert x  Empty = x 
-insert x (Node y children) = Node y (x: children)
+insert x (Node y children v) = Node y (x: children) v
 
+-- <term> ::= <id> | <int> | <paren_expr>
+pterm :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
+pterm [] = (Empty, [])
+pterm t@((toks, str):ts) 
+    | trace ("pterm " ++ show t) False = undefined 
+    | (toks == ID)  = (Node VAR [] str, ts) 
+    | (toks == INT) = (Node CST [] str, ts) 
+    | otherwise     = (Node SET [] str, (ts))
+
+-- <sum> ::= <term> | <sum> "+" <term> | <sum> "-" <term>
+psum :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
+psum [] = (Empty, [])
+psum ((toks, str):ts) 
+    | otherwise = pterm ((toks, str):ts) 
+
+-- <test> ::= <sum> | <sum> "<" <sum>
+ptest :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
+ptest [] = (Empty, [])
+ptest ((toks, str):ts) 
+     -- | (toks == LESS) = (Node LESSTHAN [] "", ts) 
+     | otherwise = psum ((toks, str):ts) 
+
+-- <expr> ::= <test> | <id> "=" <expr>
 expr :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
 expr [] = (Empty, [])
-expr ((toks, str):ts)
-    | otherwise = (Node EXPR [], ((toks, str):ts))
+expr t@((toks, str):ts)
+    | trace ("expr " ++ show t) False = undefined 
+    -- | (toks /= ID) = 
+    --     let (n, after) = ptest ((toks, str):ts)
+    --     in  (n, after)
+    | otherwise =  
+        let na@(n, after) = ptest ((toks, str):ts)
+            ast = insert n (fst (expr after))
+        in trace ("expr na: " ++ show na) (ast, snd (expr after)) 
+--
+-- expr :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
+-- expr [] = (Empty, [])
+-- expr ((toks, str):ts) 
+--     | (toks /= ID) = ptest ((toks, str):ts)
+--     | otherwise = 
+--     -- let (n, after) = ptest ((toks, str):ts)
+--         -- (nextAST, remainingTokens) = expr after
+--     let (currentAST, afterCurrent) = ptest ((toks, str):ts)
+--         (nextAST1, afterNext1) = expr afterCurrent
+--         (nextAST2, remainingTokens) = expr afterNext1
+--     in if (toks == ID) 
+--         then let combinedAST1 = insert currentAST nextAST1
+--                  combinedAST2 = insert combinedAST1 nextAST2
+--              in (combinedAST2, remainingTokens)
+--         -- then (insert n nextAST, remainingTokens)
+--         -- else (n, ts)
+--         else (Node EMPTY [] "", ts)
+
+-- expr :: [(Toks, String)] -> (AST Sym, [(Toks, String)])
+-- expr [] = (Empty, [])
+-- expr ((toks, str):ts)
+--     | toks /= ID = 
+--         let (parsedNode, remaining) = ptest ((toks, str):ts)
+--         in (parsedNode, remaining)
+--     | toks == ID = 
+--         let (currentAST, afterCurrent) = ptest ((toks, str):ts)
+--             (nextAST1, afterNext1) = expr afterCurrent
+--             (nextAST2, remainingTokens) = expr afterNext1
+--             combinedAST1 = insert currentAST nextAST1
+--             combinedAST2 = insert combinedAST1 nextAST2
+--         in (combinedAST2, remainingTokens)
+
 
 par :: [(Toks, String)] -> AST Sym
-par [] = Node PROG []
+par [] = Node PROG [] ""
 par ((toks, str):ts)
     | (toks == SIF)    = par ts
     | (toks == SDO)    = par ts
     | (toks == SWHILE) = par ts
     | (toks == LBRA)   = par ts
+    | (toks == SEMI)   = par ts
     | otherwise = 
         let (n, after) = (expr ((toks, str):ts))
         in insert n (par after)
